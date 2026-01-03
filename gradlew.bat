7af6b75ae63443696ab2054eb5a4b86cc5e1d626
aravindh-c <61825537+aravindh-c@users.noreply.github.com>
2026-01-03 21:38:24 +0530

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,268 @@
+Embedded
+==========
+Skill :Embedded Hardware
+Experience : 3 to 8 Years
+Job Description : 
+Good knowledge of Analog and Digital Circuits OPAMP, Filters, ADC, Sensors, Digital interfaces etc.
+Experience in using Orcad tool for schematic design.
+Location :Pune
+
+
+Skill : Video Analytics
+Experience : 3 to 8 Years
+Job Description :
+Excellent skills in digital image processing, digital signal processing and machine learning
+Excellent understanding of image and video processing skills involving image analysis and transformation etc.
+Good understanding of computer vision algorithms
+Proficient in packages like Image Processing Toolbox of MATLAB, OpenCV, Scikit Image, Torch, Theano, Tensor Flow, Mlpack, Eigen etc
+Location : Pune
+
+
+Skill : C++
+Experience : 2 to 5 Years
+Job Description : 
+Embedded System: Embedded C/C++, Requirement analysis, Design, Code development, Integration testing, Unit Testing, static code analysis
+Good understanding of ECU architecture, AUTOSAR architecture
+Hands on with AUTOSAR integration
+Plant/ECU modelling experience using Tools - MATLAB/Simulink
+Location :
+Bangalore
+Hyderabad
+Pune
+
+
+Skill : ADAS
+Experience : 3 to 7 Years
+Job Description :
++3 Y exp in product development in ADAS domain
+Background in one or more of the following areas: RADAR, computer vision, sensor fusion, path planning, road model estimate, target selection, vehicle control features
+Strong knowledge of algorithm development in Simulink, Stateflow, and MATLAB scripting.
+Experience in Writing / creating system requirements.
+Knowledge of production code generation using MATLAB Coder/Simulink embedded coder.
+Proficient in CAN communication tools (CANoe, CANalyzer, etc.) dSPACE/ControlDesk
+Location : Chennai
+Pune
+
+
+
+Skill : Matlab
+Experience : 2 to 7 Years
+Job Description : Knowledge of Model Based Software Development Life Cycle (MBD- SDLC)
+V&V Experience in Matlab
+MBD Related Tools Experience
+Good understanding of C/Embedded-C source code
+Good communication skill and good team player
+Hands on experience of MIL Testing, Auto Code Generation (ACG)
+Location : Pune
+
+
+
+Skill : Functional safety
+Experience : 3 to 5 Years
+Job Description : 
+Automotive Embedded SWC Development lifecycle experience CAN/Diagnostics experience
+Hands on various microcontroller programming
+Knowledge of Various Protocols (UDS, KWP, CAN, LIN etc)
+Excellent debugging skill
+Complete understanding of ISO26262 compliance processes
+Experience of system, hardware, software development process
+Fault Tree Analysis, FMEA, Common Cause Analysis, Dependent Failure Analysis, Software Safety Mechanisms
+Location : Pune
+
+
+Skill : Validation Engineer
+Experience : 3 to 7 Years
+Job Description : Expertise in Automotive Domain
+Expert in Python, with knowledge of at least one Python web framework
+Able to create database schemas that represent and support business processes
+Strong unit test and debugging skil
+Understanding of NI LabView, Teststand
+Proficient in CAN communication tools (CANoe, CANalyzer, etc.)
+Knowledge of Various Protocols (UDS, KWP, CAN, LIN etc...)
+Location : Pune
+
+
+Skill : Telematics
+Experience : 3 to 9 Years
+Job Description : Strong knowledge of C#, OOPS Concepts, application development, multi-threading, debugging
+Good end to end application development skills
+Good knowledge of UML, System engineering
+Location : Pune
+
+
+Skill : Autosar
+Experience : 2 to 7 Years
+Job Description : Automotive Embedded system and software architecture
+Experience in AUTOSAR
+Experience in Automotive ECU software development in C
+Experience in working with AUTOSAR BSW, MCAL and OS  
+configuration and Integration with application            
+Experience in configuration of Autosar BSW stack, RTE
+Location : Chennai
+Pune
+
+
+Skill : C++ Developer
+Experience : 3 to 9 Years
+Job Description : Strong C++ coding experience
+Good Embedded experience
+Strong Analytical and Communication skills
+Experience on tools/sw – Eclipse, Perforce. QNX OS
+Location : Hyderabad
+
+
+Skill : Firmware developer
+Experience : 4+  Years
+Job Description : Experience of developing efficient, effective, multi-threaded embedded firmware on resource limited custom hardware platforms
+Strong C, C++ experience
+Experience in debugging using JTAG or similar
+Experience in code refactoring and optimization, Experience in developing unit test cases and unit testing
+Experience in protocol development over TCP/IP and Serial communication
+Location : 
+Bangalore
+Hyderabad
+Pune
+
+
+Skill : Embedded C++
+Experience : 4 to 8 Years
+Job Description : Strong C++ coding experience,Good Embedded experience,Strong Analytical and Communication skills,Experience on tools/sw – Eclipse, Perforce. QNX OS
+Location : Bangalore
+
+
+
+Skill : Infotainment Development
+Experience : 4 to 8 Years
+Job Description : Experience in C/ C++ & OOAD,Experience in developing HMI using GL Studio/ QT / EB Guide,Experience in model based development using Rational Rhapsody,Experience in HMI development for Media Player / Tuners/ DVD/ Telephone,Knowledge on various HMI architectures and tools,
+Knowledge on various HMI architectures and tools, Experience in RTOS/ Linux/ Tizen, Module lead would have experience of leading 6~8 members.
+Location : Bangalore
+Pune
+
+
+
+Skill : Infotainment Validation/Labview
+Experience : 4 to 8 Years
+Job Description : Strong Test Automation Background,  Infotainment / Embedded Systems Knowledge (Media, RF, HMI),esting (VnV) concepts, Phases,SDLC,Strong hold NI LabVIEW & TestStand Expert, 
+Location : Bangalore
+
+
+Skill : Traction Control Engineers
+Experience : 4 to 8 Years
+Job Description : Hands on experience with the design and development of control algorithms in a relevant field.
+Good working knowledge on usage of Simulink/Stateflow or similar tools.
+Location : Bangalore
+
+Skill : Vehicle Control Engineers
+Experience : 4 to 8 Years
+Job Description : Matlab/Simulink/Stateflow, Lab View, MS Visio, DOORs, Protocols like- Ethernet, Modbus, Arcnet, CAN, 802.3, 802.11.
+Experience in System/sub-system engineering design activities and specification generation or System Integration for locomotive/automotive/marine control systems
+Location : Bangalore
+
+
+
+Skill : VxWorks, C++, UML, RTOS
+Experience : 3 to 11 Years
+Job Description :Development experience in C++, specifically STL, templates and structures
+Knowledge of RTOS, specifically vxWorks.
+Working Experience in Embedded Systems.
+Worked on GUI development using Tilcon.
+Excellent analytical and problem solving skills.
+Unit Testing and Module Testing knowledge.
+UML diagrams understanding with tools like IBM Rhapsody.
+Unix OS knowledge.
+Location : Pune
+
+==================================================================================================
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+
+Skill :
+Experience :
+Job Description :
+Location :
+
+
+Skill :
+Experience :
+Job Description :
+Location :
